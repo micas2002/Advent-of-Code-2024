93e3ad9db867d326b69ee3884ab415ebf5968da6
micas2002 <miguelbernardinosousa2002@gmail.com>
2024-12-10 11:14:12 +0000

    tried to fix once more, still not working as intended

--- day02/puzzle01.py
+++ day02/puzzle01.py
@@ -1,53 +1,56 @@
 import copy
 
 # checks decrease sequence
-def checkDecrease(numbers):
-	i = 0
-	while i in range(len(numbers)):
-		if (i == len(numbers) - 1):
+def	checkDecrease(numbers):
+	for i in range(len(numbers)):
+		if i == len(numbers) - 1:
 			break
+		# if first number smaller than the second number or if difference between numbers greater than 3 return false
+		if numbers[i] <= numbers[i + 1] or numbers[i] - numbers[i + 1] > 3:
+			return False
 
-		if numbers[i] < numbers[i + 1] or numbers[i] - numbers[i + 1] > 3:
-			return 0
-		i += 1
-	return 1
+	return True
 
-# check increase sequence
-def checkIncrease(numbers):
-	i = 0
-	while i in range(len(numbers)):
-		if (i == len(numbers) - 1):
+# checks increase sequence
+def	checkIncrease(numbers):
+	for i in range(len(numbers)):
+		if i == len(numbers) - 1:
 			break
+		# if first number greater than the second number or if difference between numbers greater than 3 return false
+		if numbers[i] >= numbers[i + 1] or numbers[i + 1] - numbers[i] > 3:
+			return False
 
-		if numbers[i] > numbers[i + 1] or numbers[i + 1] - numbers[i] > 3:
-			return 0
-		i += 1
-	return 1
+	return True
 
 # check if sequence in initialy increasing or decreasing
 def	isSafe(numbers):
-	result = 0
+	if len(numbers) < 2:
+		return True
 	numbersValue = copy.deepcopy(numbers) # deep copy
+	result = 0
 
 	# check complete list
-	if numbers[0] > numbers[1]:
-		result = checkDecrease(numbers)
-	elif numbers[0] < numbers[1]:
-		result = checkIncrease(numbers)	
+	if numbersValue[0] > numbersValue[1]:
+		result = checkDecrease(numbersValue)
+	elif numbersValue[0] < numbersValue[1]:
+		result = checkIncrease(numbersValue)
 
 	if result == 1: 
 		return True
 
 	# check list removing one number at a time
-	for i in range(len(numbers)):
+	for i in range(len(numbers) - 1):
 		numbersValue.pop(i)
+		if len(numbersValue) < 2:
+			return True
+		print(numbersValue)
 		if numbersValue[0] > numbersValue[1]:
 			result = checkDecrease(numbersValue)
 		elif numbersValue[0] < numbersValue[1]:
 			result = checkIncrease(numbersValue)
-		numbersValue = copy.deepcopy(numbers)
 		if result == 1:
 			return True
+		numbersValue = copy.deepcopy(numbers)
 
 	return False
 
@@ -67,7 +70,6 @@ def myFunction():
 
 	safe = 0
 	for i in input1:
-		control = 1
 		numbers = i.split() # split input into list of numbers
 		numbers =  [int(number) for number in numbers]
 		safe += isSafe(numbers)
